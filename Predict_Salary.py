46ada86061fa2ba172ff2ad5957ee2911bf095cb
palaniarunc <69773441+palaniarunc@users.noreply.github.com>
2026-03-04 10:32:00 -0500

    k2 vs r^2 graph

--- Predict_Salary.py
+++ Predict_Salary.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import r2_score, mean_absolute_error
 from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
 
 
 data_path = "job_placement_encoded_official.csv"
@@ -74,3 +75,25 @@ print("Best R2 Score:", best_r2)
 best_pred = best_pipe.predict(X_test)
 best_mae = mean_absolute_error(y_test, best_pred)
 print("Best Test MAE:", best_mae)
+
+
+#Graphing Below
+
+
+#K vs R^2 
+plt.figure()
+plt.plot(list(k_val), r2_scores)
+plt.xlabel("K value")
+plt.ylabel("R^2 Score")
+plt.title("K Value vs R^2 Score")
+plt.scatter(best_k, best_r2, color = "red", label="Best K")
+# annotate the best result (only k and r2 coordinates)
+plt.annotate(
+    f"Best k: {best_k}\nR^2: {best_r2:.4f}\nBest mae: {best_mae:.4f}",
+    (best_k, best_r2),  # xy should be a 2-tuple
+    textcoords="offset points",
+    xytext=(0,-40),
+    ha="center"
+)
+
+plt.show()
