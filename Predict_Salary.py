7919f5a763e0d637ad379dfbde7f7180e02eb71d
palaniarunc <69773441+palaniarunc@users.noreply.github.com>
2026-03-04 08:20:08 -0500

    Created Predict_Salary.py + Imports

---
+++ Predict_Salary.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import r2_score, mean_absolute_error
+
