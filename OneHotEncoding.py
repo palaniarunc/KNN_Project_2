f3f8985d2cefb754a5af04c2440317b5686fa20d
palaniarunc <69773441+palaniarunc@users.noreply.github.com>
2026-03-04 07:44:02 -0500

    Created and update on OneHotEncoding

--- OneHotEncoding.py
+++ OneHotEncoding.py
@@ -3,3 +3,11 @@ import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 
 
+df = pd.read_csv("job_placement_rcolumns.csv")
+
+categorical_cols = ['gender', 'degree', 'stream', 'college_name', 'placement_status']
+df_encoded = pd.get_dummies(df, columns=categorical_cols, drop_first = False)
+
+boolean_cols = df_encoded.select_dtypes(include=['bool']).columns
+df_encoded[boolean_cols] = df_encoded[boolean_cols].astype(int)
+
