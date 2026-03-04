424128e853f399ae6d7eda9c14ae56947cab12b6
palaniarunc <69773441+palaniarunc@users.noreply.github.com>
2026-03-04 07:38:15 -0500

    Removed columns code works

--- Removing_Columns.py
+++ Removing_Columns.py
@@ -5,4 +5,8 @@ col_drop = ['id','name','degree']
 df = df.drop(columns=col_drop)
 
 
-output_path = "/"
+output_path = "/Users/palani/Documents/GitHub/KNN_Project_2/job_placement_rcolumns.csv"
+df.to_csv(output_path, index=False)
+print("Encoded data saved to,", output_path)
+
+
